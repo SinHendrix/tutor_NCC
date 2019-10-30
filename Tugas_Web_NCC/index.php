d2f69e0bb0cbe356cdd7183f8067d5b953811bf0
SinHendrix <56729915+SinHendrix@users.noreply.github.com>
2019-10-31 00:28:11 +0700

    Add files via upload

---
+++ Tugas_Web_NCC/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title> Bibil - Find Your Hotel </title>
+              <link rel="icon" href="icon.png" type="image/png" sizes="10x10"/>
+              <meta name = "description" content = "Hotel Booking"  />
+              <meta name = "keywords" content = "Hotels, Booking, Vacation"  />
+              <meta name = "author" content = "SinHendrix"  />
+              <link rel="stylesheet" href="style.css" type="text/css" />
+
+
+
+
+        </head>
+
+        <body>
+
+            <div id ="header" > 
+                <div id="logo"><img src = "img/2.jpg" width = "237px"></div>
+                <div id= "nav">
+                    <ul>
+                        <li><a href = "index.php"> Home </a></li>
+                        <li><a href = "about.html"> About </a></li>
+                        <li><a href = "register.html"> Register </a></li>
+                        <li><a href = "login.html"> Login </a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div id ="wrapper" >wrapper
+
+            <div id ="main" > <img src = "img/1.jpg"/> </div>
+
+            <table id="table1" cellspacing = "50">
+                <tr>
+                    <td><h3>Home</h3>"Lalala..." <a href="index.php">more</a></td>
+                    <td><h3>About</h3>"Lalala..." <a href="about.html">more</a></td>
+                    <td><h3>Register</h3>"Lalala..." <a href="register.php">more</a></td>
+                    <td><h3>Login</h3>"Lalala..." <a href="login.php">more</a></td>
+                </tr>
+            </table>
+
+            <div id ="footer" > &copy; Project Web Camin NCC </div>
+
+            </div>
+
+
+
+        </body>
