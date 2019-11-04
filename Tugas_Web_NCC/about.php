0b23b2764ce50a91599ef3ab115c820a6164cafd
SinHendrix <56729915+SinHendrix@users.noreply.github.com>
2019-11-04 20:58:43 +0700

    second

---
+++ Tugas_Web_NCC/about.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title> About </title>
+              <link rel="icon" href="icon.png" type="image/png" sizes="10x10"/>
+              <meta name = "description" content = "Hotel Booking"  />
+              <meta name = "keywords" content = "Hotels, Booking, Vacation"  />
+              <meta name = "author" content = "SinHendrix"  />
+              <link rel="stylesheet" href="style.css" type="text/css" />
+
+
+
+
+        </head>
+
+        <body>
+
+            <div id ="header" > 
+                <div id="logo"><a href="index.php"><img src = "img/4.png" width = "120px" height = "120px"/></a></div>
+                <div id= "nav">
+                    <ul>
+                        <li><a href = "index.php"> Home </a></li>
+                        <li><a href = "about.php"> About </a></li>
+                        <li><a href = "register.php"> Register </a></li>
+                        <li><a href = "login.php"> Login </a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div id ="wrapper" >
+
+            <div id ="main" > <h3>About Page</h3> </div>
+
+
+            <div id ="footer" ><br/> &copy; Project Web Camin NCC </div>
+
+            </div>
+
+
+
+        </body>
--- Tugas_Web_NCC/index.php
+++ Tugas_Web_NCC/index.php
@@ -16,31 +16,31 @@
         <body>
 
             <div id ="header" > 
-                <div id="logo"><img src = "img/2.jpg" width = "237px"></div>
+                <div id="logo"><a href="index.php"><img src = "img/4.png" width = "120px" height = "120px"/></a></div>
                 <div id= "nav">
                     <ul>
                         <li><a href = "index.php"> Home </a></li>
-                        <li><a href = "about.html"> About </a></li>
-                        <li><a href = "register.html"> Register </a></li>
-                        <li><a href = "login.html"> Login </a></li>
+                        <li><a href = "about.php"> About </a></li>
+                        <li><a href = "register.php"> Register </a></li>
+                        <li><a href = "login.php"> Login </a></li>
                     </ul>
                 </div>
             </div>
 
-            <div id ="wrapper" >wrapper
+            <div id ="wrapper" >
 
             <div id ="main" > <img src = "img/1.jpg"/> </div>
 
             <table id="table1" cellspacing = "50">
                 <tr>
                     <td><h3>Home</h3>"Lalala..." <a href="index.php">more</a></td>
-                    <td><h3>About</h3>"Lalala..." <a href="about.html">more</a></td>
+                    <td><h3>About</h3>"Lalala..." <a href="about.php">more</a></td>
                     <td><h3>Register</h3>"Lalala..." <a href="register.php">more</a></td>
                     <td><h3>Login</h3>"Lalala..." <a href="login.php">more</a></td>
                 </tr>
             </table>
 
-            <div id ="footer" > &copy; Project Web Camin NCC </div>
+            <div id ="footer" ><br/> &copy; Project Web Camin NCC </div>
 
             </div>
 
